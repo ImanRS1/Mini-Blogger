e339a8b21afe8e21e9401ef71dfbe8286fd4c0ec
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-09-17 08:23:45 +0200

    init routering set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'moongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -12,8 +16,24 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://ImanRS1:ImanRS1@cluster0.hm0ew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+
+//mongoose.set('useFindAndModify', false)
+
+mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+/*
+mongoose.connect(CONNECTION_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false)
+mongoose.set('useFindAndModify', false);
+
+await mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true, // <-- no longer necessary
+  useUnifiedTopology: true // <-- no longer necessary
+});
+
+*/
