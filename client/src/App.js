332eb1793ad715738fa57d405ebe30e8b1bf98ff
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-09-22 08:17:37 +0200

    images folder setup, basic linking

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import blogg from "./images/blogg.png"
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Mini Blogger</Typography>
+                <img src={blogg} alt='blog' height="60" />
+            </AppBar>
+        </Container>
     );
 };
 
