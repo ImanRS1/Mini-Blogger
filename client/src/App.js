19b4ed78fa1eea5dadac76507a203a7dd3d61c6d
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-09-28 08:18:01 +0200

    init layout of frontend app.js structured

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,18 @@ const App = () => {
                 <Typography variant='h2' align='center'>Mini Blogger</Typography>
                 <img src={blogg} alt='blog' height="60" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" align-items="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 };
