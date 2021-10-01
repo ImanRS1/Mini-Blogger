1de29ab01c785873679b49ac0e3e05447d98c6d3
Iman Reza Soltani <iman.rezasoltani@gmail.com>
2021-10-01 08:05:44 +0200

    init settup of Posts/Post, imports to app.js added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import blogg from "./images/blogg.png"
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
     return (
